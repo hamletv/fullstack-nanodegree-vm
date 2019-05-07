8c847c09f728d6c3f98c84f93fc8f673d836e0eb
Hamlet <hamlet.villa@gmail.com>
2019-05-07 14:46:10 -0400

    CRUD decorators added

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,10 +1,50 @@
 from flask import Flask     # Flask class from flask library
 app = Flask(__name__)       # create instance of class with name of application
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 @app.route('/')             # decorator - if either routes (/ or /hello) entered into browser
-@app.route('/hello')        # function HelloWorld will run.
-def HelloWorld():
-    return "Hello World"
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+
+    output = ""
+    for item in items:
+        output += item.name
+        output += "</br>"
+        output += item.description
+        output += "</br>"
+        output += item.price
+        output += "</br>"
+        output += "</br>"
+    return output
+
+# Task 1: Create route for newMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+@app.route('restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':  # if executing from python interpreter run normally on port 5000
     app.debug = True        # server reloads anytime code changes
